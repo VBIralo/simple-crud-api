f2b0d08296cbf10303e16b0898201485b25ca3ae
VBiralo <mr.biralo@yandex.ru>
2021-11-27 20:50:41 +0300

    feat: added the third test script

--- test/app.test.js
+++ test/app.test.js
@@ -197,3 +197,124 @@ describe('Сценарий #2', function () {
       });
   });
 });
+
+describe('Сценарий #3', function () {
+  it('1. GET-запросом получаем все объекты (ожидается три записи из предыдущих сценариев, и первая удаленная null, и это не баг)', function (done) {
+    request(app)
+      .get('/person')
+      .expect(200)
+      .expect([null, { "name": "Max", "age": 1, "hobbies": [""], "id": 2 }, { "name": "Henry", "age": 20, "hobbies": [], "id": 3 }])
+      .end(function (err, res) {
+        if (err) return done(err);
+        done();
+      });
+  });
+
+  it('2. PUT-запросом пытаемся обновить объект (ожидается ответ, содержащий обновленный объект с тем же id)', function (done) {
+    request(app)
+      .put('/person/3')
+      .send({ "name": "Mike", "age": 10, "hobbies": ["sport"] })
+      .expect(200)
+      .expect({ "name": "Mike", "age": 10, "hobbies": ["sport"], "id": 3 })
+      .end(function (err, res) {
+        if (err) return done(err);
+        done();
+      });
+  });
+
+  it('3. PUT-запросом пытаемся обновить удаленный объект (ожидается ошибка)', function (done) {
+    request(app)
+      .put('/person/1')
+      .send(postData2)
+      .expect(404)
+      .expect({ message: "This person doesn't exist." })
+      .end(function (err, res) {
+        if (err) return done(err);
+        done();
+      });
+  });
+
+  it('4. POST запрос на несуществующий ресурс (ожидается ошибка)', function (done) {
+    request(app)
+      .post('/non/existing/route')
+      .send(postData2)
+      .expect(404)
+      .expect({ "message": "Route not found. Use only /person or /person/{personID}." })
+      .end(function (err, res) {
+        if (err) return done(err);
+        done();
+      });
+  });
+
+  it('5. Несуществующий метод (COPY) запроса (ожидается ошибка)', function (done) {
+    request(app)
+      .copy('/person')
+      .expect(404)
+      .expect({ "message": "Route not found. Use only /person or /person/{personID}." })
+      .end(function (err, res) {
+        if (err) return done(err);
+        done();
+      });
+  });
+
+  it('6. PUT-запросом пытаемся обновить только один параметр (ожидается ответ, содержащий обновленный объект с тем же id)', function (done) {
+    request(app)
+      .put('/person/3')
+      .send({ "name": "Mike" })
+      .expect(200)
+      .expect({ name: 'Mike', age: 10, hobbies: ['sport'], id: 3 })
+      .end(function (err, res) {
+        if (err) return done(err);
+        done();
+      });
+  });
+
+  it('7. GET-запросом пытаемся получить измненный объект по его id (ожидается измененный объект)', function (done) {
+    request(app)
+      .get('/person/3')
+      .expect(200)
+      .expect({ name: 'Mike', age: 10, hobbies: ['sport'], id: 3 })
+      .end(function (err, res) {
+        if (err) return done(err);
+        done();
+      });
+  });
+
+  it('8. POST-запросом создается новый объект (ожидается ответ, содержащий свежесозданный объект)', function (done) {
+    request(app)
+      .post('/person')
+      .send(postData4)
+      .expect(201)
+      .expect({ "id": 4, "name": "Max", "age": 1, "hobbies": [""] })
+      .end(function (err, res) {
+        if (err) return done(err);
+        done();
+      });
+  });
+
+  it('9. POST-запросом создается новый объект (ожидается ответ, содержащий свежесозданный объект)', function (done) {
+    request(app)
+      .post('/person')
+      .send(postData5)
+      .expect(201)
+      .expect({ "id": 5, "name": "Henry", "age": 20, "hobbies": [] })
+      .end(function (err, res) {
+        if (err) return done(err);
+        done();
+      });
+  });
+
+  it('10. GET-запросом получаем все объекты (ожидается 3 записи)', function (done) {
+    request(app)
+      .get('/person')
+      .expect(200)
+      .end(function (err, res) {
+        if (err) {
+          return done(err);
+        }
+        expect(res.body).to.have.length(5);
+        return done();
+      });
+  });
+
+});
